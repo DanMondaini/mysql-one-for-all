fb128974d7c5d59a98aed13aed1fcd6b0fcadbb6
DanMondaini <danielmondaini@outlook.com>
2022-02-04 16:53:36 -0300

    Req 6 corrigido agora sim

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT
-	MIN(p.preco) AS faturamento_minimo,
-	MAX(p.preco) AS faturamento_maximo,
-	FORMAT(AVG(p.preco), 2) AS faturamento_medio,
-	SUM(p.preco) AS faturamento_total
+	FORMAT(MIN(p.preco), 2) AS faturamento_minimo,
+    FORMAT(MAX(p.preco), 2) AS faturamento_maximo,
+    FORMAT(AVG(p.preco), 2) AS faturamento_medio,
+    FORMAT(SUM(p.preco), 2) AS faturamento_total
 FROM SpotifyClone.plano AS p
 INNER JOIN SpotifyClone.usuario AS u
-ON u.plano_id = p.plano_id;
+ON u.plano_id = p.id;
