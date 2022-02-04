9c2734ada1d0118c491d3812dfe7a21174d993de
DanMondaini <danielmondaini@outlook.com>
2022-02-04 15:55:38 -0300

    Req 5 corrigido

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,9 @@
 SELECT
 	  c.nome AS cancao,
-    COUNT(c.id) AS reproducao
+    COUNT(c.id) AS reproducoes
 FROM SpotifyClone.cancao AS c
 INNER JOIN SpotifyClone.historico_reproducao AS h
 ON c.id = h.cancao_id
 GROUP BY c.nome
-ORDER BY reproducao DESC, c.nome
+ORDER BY reproducoes DESC, c.nome
 LIMIT 2;
